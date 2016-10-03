d48d6695e2e346e2f752c92b324060a97c8de569
pasaunders <kaorti@gmail.com>
2016-10-03 14:54:34 -0700

    added marker

--- scripts/models/mapModel.js
+++ scripts/models/mapModel.js
@@ -1,3 +1,17 @@
-(function(module){
-  
-})(window);
+//(function(module){
+  var locations = {};
+  locations.allLocations = [];
+
+  function initMap (){  //initalizes map.
+    var map =  new google.maps.Map(document.getElementById('mapBox'),{
+      center: {lat: 47.6062, lng: 122.3321 },
+      zoom: 10
+    });
+  };
+
+  var marker = new google.maps.Marker({
+    position: {/* insert position data from selected locations */},
+    map: map
+  });
+
+// })(window);
