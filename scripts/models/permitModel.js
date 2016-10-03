9b6d50b556cb350ea567ec5971434b13e35c1aa6
iamrobinhood12345 <swordsshadow@Williams-MacBook-Pro.local>
2016-10-03 11:54:02 -0700

    commented each line of permitModel.js. Included TODO for wrapping in module. permitModel.js may need to include get requests for specific filtering.

--- scripts/models/permitModel.js
+++ scripts/models/permitModel.js
@@ -1,9 +1,21 @@
 // (function(module) {
+
+  // TODO: wrap this in module.
+
+  // permits object to contain permits array,
+  // get request to populate array,
+  //
   var permits = {};
+
+  // permits.all will contain permit data from endpoint
+  // https://data.seattle.gov/resource/i5jq-ms7b.
+  // get request to populate permits.all with
+  // permits data
 
   permits.all = [];
 
-  tryArray = [];
+  // get requst to populate permits.all with
+  // permits data. endpoint https://data.seattle.gov/resource/i5jq-ms7b.
 
   permits.requestPermits = function() {
     $.get('https://data.seattle.gov' +
@@ -12,6 +24,8 @@
       permits.all = data;
     });
   };
+
+  // temporary invocation.
 
   permits.requestPermits();
 
