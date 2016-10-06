d337cf60b0556008b96c53543142470f422cb706
Jonathan <jonbustamonte@icloud.com>
2016-10-06 15:12:17 -0700

    code cleanup

--- scripts/controllers/requestController.js
+++ scripts/controllers/requestController.js
@@ -1,33 +1,9 @@
 (function(module) {
-
-  // requestController is an object that contains
-  // functions for generating a sql query based
-  // on user input. a sql query string is generated
-  // from values in the radio button forms and select
-  // option tags in index.html. these are sent to
-  // permitModel.js where an ajax call gets our
-  // data.
-
   requestController = {};
-
-  // these vars are pieces used to build a sql query.
-  // sqlSelect is a defined select phrase in sql.
-  // sqlTail is a default tail to go on the end of
-  // the sql query. sqlAND stores a value to be placed
-  // between sql conditions in the sql query. sqlTail
-  // currently specifies a lower bound for date.
 
   var sqlSelect = '?$select=*&$where=';
   var sqlTail = 'application_date%20>%20%272011-01-01T00:00:00%27&$order=application_date DESC';
   var sqlAND = '%20AND%20';
-
-  // This function returns a string of sql that
-  // contains the conditions for the sql query to be
-  // sent to permitModel.js. The conditinos are appended
-  // to each other connected by var sqlAND to form
-  // valid sql. in the case that a lower bound is specified
-  // for date, the default sqlTail is replaced by the new
-  // date lower bound.
 
   sqlConditions = function() {
     var sqlConditions = '';
@@ -58,23 +34,12 @@
     return sqlConditions;
   };
 
-  // This function generates an sql query by returning
-  // a string that is the combination of a select string
-  // (sqlSelect), a conditions string (sqlConditions())
-  // and an sql tail (sqlTail).
-
   sqlQuery = function() {
     var sqlString = sqlSelect +
     sqlConditions() +
     sqlTail;
     return sqlString;
   };
-
- // The query is generated and is given to
- // permits.getRequestedUserData() in
- // permitModel.js. permits.getRequestedUserData() is
- // called in the Page next(); function at the end of the
- // function below, requestController.controlRequest.
 
   requestController.controlRequest = function(ctx, next) {
     ctx.sql = sqlQuery();
